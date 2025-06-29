9bfb0d3ca293ee59e9caa85ec6c7ba736997e4f9
Ashwin <ashwinsp2405@gmail.com>
2025-06-29 22:29:49 +0530

    Day 4: Added background color changer with JS

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function changeMessage() {
   const greeting = document.getElementById("greeting");
   greeting.innerText = "You're doing amazing! 🎯 Keep going!";
 }
+const colors = ["#f5f5f5", "#ffd6e0", "#c3f0ca", "#ffecd1", "#d0e8f2", "#fce38a"];
+
+function changeColor() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  const randomColor = colors[randomIndex];
+  document.body.style.backgroundColor = randomColor;
+
+  console.log("Background changed to:", randomColor);
+}
